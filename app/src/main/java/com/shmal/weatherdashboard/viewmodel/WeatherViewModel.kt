3e3f20f903ff49af8713a244cad79b4fd5b9c3e0
shmal-chel <shcmalivan@gmail.com>
2026-04-06 22:57:28 +0300

    Step 5: Create ViewModel with sequential loading

---
+++ app/src/main/java/com/shmal/weatherdashboard/viewmodel/WeatherViewModel.kt
@@ -0,0 +1,46 @@
+package com.shmal.weatherdashboard.viewmodel
+
+import android.util.Log.e
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.shmal.weatherdashboard.data.WeatherData
+import com.shmal.weatherdashboard.data.WeatherRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class WeatherViewModel : ViewModel() {
+    private val repository = WeatherRepository()
+    private val _weatherState = MutableStateFlow(WeatherData())
+    val weatherState: StateFlow<WeatherData> = _weatherState.asStateFlow()
+    init {
+        loadWeatherData()
+
+    }
+    fun loadWeatherData(){
+        viewModelScope.launch {
+            _weatherState.value = _weatherState.value.copy(
+                isLoading = true,
+                error = null
+            )
+            try {
+                val temperature = repository.fetchTemperature()
+                _weatherState.value = _weatherState.value.copy(temperature = temperature)
+
+                val humidity = repository.fetchHumidity()
+                _weatherState.value = _weatherState.value.copy(humidity = humidity)
+
+                val windSpeed = repository.fetchWindSpeed()
+                _weatherState.value = _weatherState.value.copy(windSpeed = windSpeed)
+
+                _weatherState.value = _weatherState.value.copy(isLoading = false)
+            } catch (e: Exception) {
+                _weatherState.value = _weatherState.value.copy(
+                    isLoading = false,
+                    error = "Ошибка загрузки: ${e.message}"
+                )
+            }
+        }
+    }
+}
